cc4c3017dfc7bdc1b93575447565fccd85397afb
donco <colin.donald@outlook.com>
2019-07-11 08:58:00 -0700

    added bacteria analysis functions

--- R/bact_shell_harvest.R
+++ R/bact_shell_harvest.R
@@ -1,18 +1,27 @@
+#' Bacteria Shell Harvest Analysis
+#'
+#' Assesses Ecoli data against the standard
+#' @param df dataframe with Ecoli data
+#' @param criteria POSIXCT column name containing criteria values
+#' @return a dataframe with relevant Ecoli criteria and excursion variables added
+#' @export
+#' @examples function(df = your_ecoli_data, criteria = "bact-shell-crit")
+
 Shell_Harvest <- function(df, criteria = "bact_crit_percent") {
-  
+
   print("Begin shellfish harvesting analysis")
-  
+
   Perc_Crit <- as.symbol(criteria)
-  
+
   shell_harvest <- df %>%
     filter(BacteriaCode == 3,
            Char_Name == "Fecal Coliform") %>%
     mutate(perc_exceed = ifelse(Result_cen > !!Perc_Crit, 1, 0))
-  
+
   if(nrow(shell_harvest) == 0) {
     stop("No available data")
   }
-  
+
   shell_harvest_analysis <- shell_harvest %>%
     group_by(MLocID, OWRD_Basin) %>%
     summarise(num_samples = n(),
@@ -21,34 +30,7 @@ Shell_Harvest <- function(df, criteria = "bact_crit_percent") {
               Perc_Crit = first(!!Perc_Crit),
               SS_Crit = first(SS_Crit)
     )
-  
+
   return(shell_harvest_analysis)
-  
-  # # Data review -------------------------------------------------------------
-  # 
-  # 
-  # IR_export(shell_harvest, "Parameters/Bacteria/Data Review", "Bacteria_Shell_harvest", "data" )
-  # 
-  # 
-  # shell_harvest_summary <- shell_harvest_analysis %>%
-  #   mutate(IR_category = ifelse((!is.na(median) & median > SS_Crit) | 
-  #                                 (num_samples >= 10 & num_exceed/num_samples > 0.10) |
-  #                                 (num_samples >= 5 & num_samples <= 9 &  num_exceed >= 1 ), "Cat5", 
-  #                               ifelse(num_samples < 5 & num_exceed == 0, "Cat3", 
-  #                                      ifelse(num_samples < 5 & num_exceed > 0, "Cat3b", 
-  #                                             ifelse((!is.na(median) & median < SS_Crit & num_exceed/num_samples <= 0.10 & num_samples >= 10) |
-  #                                                      (!is.na(median) & median < SS_Crit & num_samples >= 5 & num_samples <= 9 & num_exceed == 0), "Cat2", "ERROR" ))) ))
-  # 
-  # 
-  # # mutate(Cat5 = ifelse((!is.na(median) & median > SS_Crit) | 
-  # #                      (num_samples >= 10 & num_exceed/num_samples > 0.10) |
-  # #                      (num_samples >= 5 & num_samples <= 9 &  num_exceed >= 1 ), 1, 0 ),
-  # #        Cat3 = ifelse(num_samples < 5 & num_exceed == 0, 1, 0 ),
-  # #        Cat3b = ifelse(num_samples < 5 & num_exceed > 0, 1, 0 ),
-  # #        Cat2 = ifelse((!is.na(median) & median < SS_Crit & num_exceed/num_samples <= 0.10 & num_samples >= 10) |
-  # #                      (!is.na(median) & median < SS_Crit & num_samples >= 5 & num_samples <= 9 & num_exceed == 0), 1, 0)
-  # #        )
-  
-  
-  
+
 }
