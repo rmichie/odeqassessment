99c6430988e46733b567d320bc8b777e5044864e
donco <colin.donald@outlook.com>
2019-07-16 09:51:00 -0700

    Updated assessments without criteria

--- R/TP_assessment.R
+++ R/TP_assessment.R
@@ -1,17 +1,19 @@
 #' Assess TP data
-#' 
+#'
 #' Asssesses TP data against the appropriate criteria and produces columns for excursions
 #' @param TP_data dataframe with TP data and criteria values
 #' @return dataframe with the following columns added; 'excursion_cen'
 #' @export
 #' @examples
 #' TP_assessment(TP_data = "your-TP-data")
-#' 
+#'
 
 TP_assessment <- function(TP_data) {
   TP_summary <- TP_data %>%
-    mutate(excursion_cen = ifelse(!is.na(TP_crit) & Result_cen > TP_crit, 1, 0)
+    mutate(excursion_cen = if_else(!is.na(TP_crit),
+                                   if_else(Result_cen > TP_crit, 1, 0),
+                                   NaN)
     )
-  
+
   return(TP_summary)
 }
--- R/TSS_assessment.R
+++ R/TSS_assessment.R
@@ -1,17 +1,19 @@
 #' Assess TSS data
-#' 
+#'
 #' Asssesses TSS data against the appropriate criteria and produces columns for excursions
 #' @param TSS_data dataframe with TSS data and criteria values
 #' @return dataframe with the following columns added; 'excursion_cen'
 #' @export
-#' @examples 
+#' @examples
 #' TP_assessment(TSS_data = "your-TSS-data")
-#' 
+#'
 
 TSS_assessment <- function(TSS_data) {
   TSS_summary <- TSS_data %>%
-    mutate(excursion_cen = ifelse(!is.na(TSS_crit) & Result_cen > TSS_crit, 1, 0)
+    mutate(excursion_cen = ifelse(!is.na(TSS_crit),
+                                  if_else(Result_cen > TSS_crit, 1, 0),
+                                  NaN)
     )
-  
+
   return(TSS_summary)
 }
